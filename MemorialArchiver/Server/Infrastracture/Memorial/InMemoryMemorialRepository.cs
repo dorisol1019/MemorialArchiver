aae264b4e3b72db008aaacf46d77f4ca616281a5
dorisol <dorisol@hotmail.co.jp>
2020-02-23 15:51:36 +0900

    動作確認のためInMemoryMemorialRepository修正

--- MemorialArchiver/Server/Infrastracture/Memorial/InMemoryMemorialRepository.cs
+++ MemorialArchiver/Server/Infrastracture/Memorial/InMemoryMemorialRepository.cs
@@ -9,7 +9,7 @@ namespace Dorisol1019.MemorialArchiver.Server.Infrastracture.Memorial
 {
     public class InMemoryMemorialRepository<T> : IMemorialRepository<T>
     {
-        private Dictionary<long, T> dic = new Dictionary<long, T>();
+        private static Dictionary<long, T> dic = new Dictionary<long, T>();
         private static long id = 1;
 
         public void Create(IMemorialCreateRequest<T> request)
